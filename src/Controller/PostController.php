a05598a412fc66cbcfee979c01bb97165d67705f
Anshul Jindal <ajindal700@gmail.com>
2021-01-28 16:13:40 +0530

    todo with functional testing

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,5 +16,24 @@ class PostController extends AbstractController
     public function index()
     {
         return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/form")
+     */
+    public function form(Request $request)
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            return $this->render('form.html.twig',[
+                'form' => $form->createView()
+            ]);
+        }
+        return $this->render('form.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -30,4 +30,15 @@ class PostControllerTest extends WebTestCase
 
         $this->assertCount(1, $crawler->filter('title'));
     }
+    public function testform()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/form');
+        $form = $crawler->selectButton('Save')->form();
+        $form["task[Task]"] = "abc";
+        $form["task[Date]"] = "1 jan 2010";
+        $client->submit($form);
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+        $this->assertGreaterThanOrEqual(0,$crawler->filter('h1')->count());
+    }
 }
